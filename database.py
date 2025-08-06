5ee35f3dea56f0ce0d33e64302b54fa12faeed10
Nadil02 <nadildinsarasiriwardhana@gmail.com>
2025-08-06 06:20:07 +0530

    password to the .env

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 from langchain_community.utilities import SQLDatabase
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 server = "nadilsqlagent.database.windows.net"
 database = "AdventureWorks2019"
 username = "sqladmin"
-password = "1Aaaaaaaa"
+password = os.getenv("DB_PASSWORD")
 
 db_uri = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC Driver 17 for SQL Server"
 
